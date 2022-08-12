32aa0331b7e780aae5aca7d804cbd6de79a7a353
Eric Johnson <95862960+Eric90johnson@users.noreply.github.com>
2022-08-11 22:04:35 -0300

    Update index.js
    Metodos de retorno.

--- index.js
+++ index.js
@@ -8,16 +8,18 @@ class ContaCorrente {
     _saldo = 0;
 
     sacar(valor) {
+        
         if(this._saldo >= valor) {
             (this._saldo -= valor);
+            return valor;            
         }
     }
 
     depositar(valor){
         if(valor >0 ) {
-            this._saldo += valor;
-            console.log(this._saldo);
+           return;                     
         }
+        this._saldo += valor; 
     }
 }
 
@@ -26,11 +28,17 @@ cliente1.nome = "Ricardo";
 cliente1.cpf = 11122233309;
 
 const contaCorrenteRicardo = new ContaCorrente();
-contaCorrenteRicardo._saldo = 0; //todo campo que tiver um _ (underline) siginifica que é um campo privado
+
 contaCorrenteRicardo.agencia = 1001;
 
-contaCorrenteRicardo.depositar(200);
-contaCorrenteRicardo.sacar(50);
+contaCorrenteRicardo.depositar(100);
+contaCorrenteRicardo.depositar(100);
+contaCorrenteRicardo.depositar(100);
+const valorSacado = contaCorrenteRicardo.sacar(50);
 
-console.log(contaCorrenteRicardo);
+console.log(valorSacado);
+console.log(contaCorrenteRicardo)
+
+
+
 
