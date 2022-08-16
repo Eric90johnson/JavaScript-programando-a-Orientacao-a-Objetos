ab085e6c10104d18781ea845a9be48e64cf65459
Eric Johnson <95862960+Eric90johnson@users.noreply.github.com>
2022-08-16 20:39:03 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 import {Cliente} from "./Cliente.js"
 import {ContaCorrente} from "./ContaCorrente.js"
-import { ContaPoupanca } from "./ContaPoupanca.js";
+import {ContaPoupanca} from "./ContaPoupanca.js";
 
 const cliente1 = new Cliente("Ricardo", 11122233309);
 
@@ -10,5 +10,5 @@ contaCorrenteRicardo.sacar(100);
 
 const contaPoupanca = new ContaPoupanca(50, cliente1, 1001);
 
-console.log(ContaPoupanca);
-//console.log(contaCorrenteRicardo);
+console.log(contaPoupanca);
+console.log(contaCorrenteRicardo);
