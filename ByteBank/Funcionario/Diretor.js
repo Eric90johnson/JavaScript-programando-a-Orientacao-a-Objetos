547b4134a048751d58d75e54d3ac0ae7c3e2752d
Eric Johnson <95862960+Eric90johnson@users.noreply.github.com>
2022-08-18 20:12:09 -0300

    Criando as classes e heranças...
    Criando as classes e heranças dos Arquivos Funcionário, Diretor e Gerente

--- ByteBank/Funcionario/Diretor.js
+++ ByteBank/Funcionario/Diretor.js
@@ -1,7 +1,9 @@
-export class Diretor {
-    constructor(){
-        this.nome;
-        this.salario;
-        this.cpf
+import { 
+    Funcionario } from "./Funcionario.js";
+
+export class Diretor  extends Funcionario{
+    constructor(nome, salario, cpf){
+        super(nome, salario, cpf);
+        this._bonificacao = 1.1;
     }
 }
